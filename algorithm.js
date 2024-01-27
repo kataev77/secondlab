72a6e3a559de4b6fa40e39d6e64db28641cadfc2
Kataev <Kataev.hamzat@mail.ru>
2024-01-27 19:34:44 +0300

    b

--- algorithm.js
+++ algorithm.js
@@ -1,5 +1,5 @@
 let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-function getDayName(day) {
+function getDayName(day){
     return day
 }
 console.log(getDayName(days[6]))
