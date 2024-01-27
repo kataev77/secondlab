e9c2f0354644abb611a5ceeee83cee970f3cc3e7
Kataev <Kataev.hamzat@mail.ru>
2024-01-27 19:10:21 +0300

    в этом задании не понял я

--- algorithm.js
+++ algorithm.js
@@ -1,14 +1,6 @@
+let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
 function getDayName(day) {
-    if(typeof day !== 'number')
-    {
-        return 'error'
-    }
-
-    if(day > 0 && day < 8) {
-        let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-        return days
-    }else {
-        return 'Неверные данные на входе'
-    }
+    return day
 }
-console.log(getDayName(7))
+console.log(getDayName(days[6]))
+
