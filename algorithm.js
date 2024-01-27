f7ee2acd71af1e4bb0a009b169ee3452e6eed21e
Kataev <Kataev.hamzat@mail.ru>
2024-01-27 18:35:03 +0300

    operators

--- algorithm.js
+++ algorithm.js
@@ -1,6 +1,13 @@
 function getDayName(day) {
-    if(typeof day !== 'number') {
+    if(typeof day !== 'number')
+    {
         return 'error'
     }
+
+    if(day > 0 && day < 8) {
+        return 'Все верно'
+    }else {
+        return 'Неверные данные на входе'
+    }
 }
-console.log(getDayName('s'))
+console.log(getDayName(7))
